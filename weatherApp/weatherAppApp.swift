819c2b4f292de6ca702b5faa3018d274f5b0d2ac
ahmethedev <ahmetburakdincc@gmail.com>
2024-03-05 22:35:08 +0300

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Ahmet Burak Dinc on 5.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
